32c99627ee7c0020d2f6ea1f086ba78dd73b454b
Cleber Souza <ccs1201@gmail.com>
2023-09-10 21:07:43 -0300

    #5 - Adicionada anotação @EnableConfigurationProperties em SalaDeJogosApplication.
    Alterada a dependência do Microstream para integração com springboot no pom.xml

--- src/main/java/com/ccs/saladejogos/SalaDeJogosApplication.java
+++ src/main/java/com/ccs/saladejogos/SalaDeJogosApplication.java
@@ -2,8 +2,10 @@ package com.ccs.saladejogos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SalaDeJogosApplication {
 
     public static void main(String[] args) {
