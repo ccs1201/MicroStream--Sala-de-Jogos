b6290c3cf8303eb3aca71736a21ff140029d1f7c
Cleber Souza <ccs1201@gmail.com>
2023-09-12 20:24:01 -0300

    #22 - Implementado método para limpar e repopular a base de dados em LoadData.resetData
    Implementado endpoint patch /api/resetdate em DataBaseController

---
+++ src/main/java/com/ccs/saladejogos/controllers/DataBaseController.java
@@ -0,0 +1,25 @@
+package com.ccs.saladejogos.controllers;
+
+import com.ccs.saladejogos.configs.LoadData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/resetdata")
+@RequiredArgsConstructor
+public class DataBaseController {
+
+    private final LoadData loadData;
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.OK)
+    void resetarBase() {
+        CompletableFuture.runAsync(() -> loadData.resetData());
+    }
+}
