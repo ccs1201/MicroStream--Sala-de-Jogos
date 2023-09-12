ea56c2d070eaabac390786851114e51db74000a9
Cleber.Souza <Cleber.Souza@senior.com.br>
2023-09-12 09:02:17 -0300

    #20 - Adicionados logs ao MicroStreamRepositoryImpl

--- src/main/java/com/ccs/saladejogos/repositories/impl/MicroStreamRepositoryImpl.java
+++ src/main/java/com/ccs/saladejogos/repositories/impl/MicroStreamRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.ccs.saladejogos.repositories.MicroStreamRepository;
 import com.ccs.saladejogos.repositories.rootinstances.DataRoot;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import one.microstream.storage.types.StorageManager;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.BeanUtils;
@@ -16,6 +17,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Slf4j
 public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> implements MicroStreamRepository<T> {
 
     protected final StorageManager storageManager;
@@ -33,6 +35,7 @@ public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> imple
 
     @Override
     public long save(T entity) {
+        log.info("### Armazenando {} ###", entity.getClass().getSimpleName());
         var collection = getColletionRoot();
         if (BooleanUtils.isFalse(collection.add(entity))) {
             throw new RepositoryException(HttpStatus.BAD_REQUEST,
@@ -43,11 +46,13 @@ public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> imple
 
     @Override
     public Collection<T> findAll() {
+        log.info("### Buscando todos os registros ###");
         return getColletionRoot();
     }
 
     @Override
     public T findByID(UUID id) {
+        log.info("### Buscando pelo ID: {} ###", id);
         return getColletionRoot()
                 .stream()
                 .filter(t -> t.getId().equals(id))
@@ -59,6 +64,7 @@ public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> imple
 
     @Override
     public void deleteDeleteById(UUID id) {
+        log.info("### Removendo ID: {} ###", id);
         var toRemove = findByID(id);
         if (BooleanUtils.isFalse(getColletionRoot().remove(toRemove))) {
             throw new RepositoryException(HttpStatus.BAD_REQUEST,
@@ -68,21 +74,21 @@ public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> imple
 
     @Override
     public void delete(T entity) {
+        log.info("### Removendo {} referência: {} ###", entity.getClass().getSimpleName(), entity);
         if (BooleanUtils.isFalse(getColletionRoot().remove(entity))) {
-            throw new RepositoryException(HttpStatus.NOT_FOUND, " Objeto não encontrado.");
+            throw new RepositoryException(HttpStatus.NOT_FOUND, entity.getClass().getSimpleName() + "  não encontrado.");
         }
     }
 
     @Override
     public void deleteAll() {
+        log.info("### Removendo todos os registros ###");
         getColletionRoot().clear();
     }
 
     @Override
-    public abstract Collection<T> findByArgs(String... args);
-
-    @Override
     public T update(T entity, UUID id) {
+        log.info("### Atualizando {} com ID: {} ###", entity.getClass().getSimpleName(), id);
         var toUpdate = findByID(id);
         BeanUtils.copyProperties(entity, toUpdate);
         storageManager.store(toUpdate);
@@ -92,6 +98,7 @@ public abstract class MicroStreamRepositoryImpl<T extends SalaJogosEntity> imple
     protected abstract Collection<T> getColletionRoot();
 
     public T update(T entity) {
+        log.info("### Atualizando {} ###", entity.getClass().getSimpleName());
         storageManager.store(entity);
         return entity;
     }
